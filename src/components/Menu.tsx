6da33e4ba32a374d5cec9bc5aec9d154096cb055
Sammy1301 <samtrabajo13@gmail.com>
2024-10-11 09:43:37 -0400

    🎉 First commit

---
+++ src/components/Menu.tsx
@@ -0,0 +1,25 @@
+import DownloadIcon from "./Icons/DownloadIcon";
+
+
+const Menu = () => {
+    return (
+        <div className="w-full flex justify-center h-screen md:h-24 items-center">
+            <div className="w-full md:w-3/6 flex gap-10 justify-between items-center flex-col md:flex-row">
+                <div className="d-inline-block text-lg font-semibold">
+                    <ul className="flex gap-6 flex-col md:flex-row">
+                        <li>About me</li>
+                        <li>Skills</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+
+                <button className="bg-black hover:bg-gray-800 text-white font-semibold  py-2 px-4 rounded flex gap-2 items-center">
+                    Resume <DownloadIcon />
+                </button>
+            </div>
+        </div>
+    )
+};
+
+export default Menu;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Menu from "@/components/Menu";
+
+export default function Home() {
+  return (
+    <div>
+      <Menu />
+      
+    </div>
+  );
+}
